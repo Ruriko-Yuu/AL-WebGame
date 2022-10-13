e82ddc4933cd6cbdec1175da9fba72a386e13ed8
Ruriko <47851127+Ruriko-Yuu@users.noreply.github.com>
2022-10-13 18:33:39 +0800

    ✨react-dnd

--- src/components/inventory/index.tsx
+++ src/components/inventory/index.tsx
@@ -5,8 +5,42 @@ import { DndProvider, useDrag } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { selectCount } from "@/features/counter/counterSlice";
 import { useSelector } from "react-redux";
+
 // https://codesandbox.io/s/jovial-meadow-2lsr7i?file=/src/Box.tsx:480-683
-// https://codesandbox.io/s/jovial-meadow-2lsr7i?file=/src/Box.tsx:480-683
+
+import update from "immutability-helper";
+import { memo, useCallback } from "react";
+import { NativeTypes } from "react-dnd-html5-backend";
+
+import { Box } from "./box";
+import { Dustbin } from "./bustbin";
+import { ItemTypes } from "./itemTypes";
+
+interface DustbinState {
+  accepts: string[];
+  lastDroppedItem: any;
+}
+
+interface BoxState {
+  name: string;
+  type: string;
+}
+
+export interface DustbinSpec {
+  accepts: string[];
+  lastDroppedItem: any;
+}
+export interface BoxSpec {
+  name: string;
+  type: string;
+}
+export interface ContainerState {
+  droppedBoxNames: string[];
+  dustbins: DustbinSpec[];
+  boxes: BoxSpec[];
+}
+
+
 const InventoryContainer = styled.div`
   ul {
     margin: 0 auto;
@@ -53,16 +87,73 @@ export const Inventory: FC<{
     setContainerSpace(list);
   }, [containerNum]);
 
+  const [dustbins, setDustbins] = useState<DustbinState[]>([
+    { accepts: [ItemTypes.GLASS], lastDroppedItem: null },
+    { accepts: [ItemTypes.FOOD], lastDroppedItem: null },
+    {
+      accepts: [ItemTypes.PAPER, ItemTypes.GLASS, NativeTypes.URL],
+      lastDroppedItem: null
+    },
+    { accepts: [ItemTypes.PAPER, NativeTypes.FILE], lastDroppedItem: null }
+  ]);
+
+  const [boxes] = useState<BoxState[]>([
+    { name: "Bottle", type: ItemTypes.GLASS },
+    { name: "Banana", type: ItemTypes.FOOD },
+    { name: "Magazine", type: ItemTypes.PAPER }
+  ]);
+
+  const [droppedBoxNames, setDroppedBoxNames] = useState<string[]>([]);
+
+  function isDropped(boxName: string) {
+    return droppedBoxNames.indexOf(boxName) > -1;
+  }
+
+  const handleDrop = useCallback(
+    (index: number, item: { name: string }) => {
+      const { name } = item;
+      setDroppedBoxNames(
+        update(droppedBoxNames, name ? { $push: [name] } : { $push: [] })
+      );
+      setDustbins(
+        update(dustbins, {
+          [index]: {
+            lastDroppedItem: {
+              $set: item
+            }
+          }
+        })
+      );
+    },
+    [droppedBoxNames, dustbins]
+  );
+
   return (
     <InventoryContainer>
       <DndProvider backend={HTML5Backend}>
-        <ul style={{ width: `${(size ? size[0] : 6) * 60 }px`, height: `${(size ? size[1] : 5) * 60 }px`}}>
+        <div style={{ overflow: "hidden", clear: "both" }}>
+          {dustbins.map(({ accepts, lastDroppedItem }, index) => (
+            <Dustbin
+              accept={accepts}
+              lastDroppedItem={lastDroppedItem}
+              onDrop={(item) => handleDrop(index, item)}
+              key={index}
+            />
+          ))}
+        </div>
+
+        <div style={{ overflow: "hidden", clear: "both" }}>
+          {boxes.map(({ name, type }, index) => (
+            <Box name={name} type={type} key={index} isDropped={false} />
+          ))}
+        </div>
+        {/* <ul style={{ width: `${(size ? size[0] : 6) * 60 }px`, height: `${(size ? size[1] : 5) * 60 }px`}}>
           {containerSpace.map((ele, idx) => (
             <li key={idx}>
               <p>1</p>
             </li>
           ))}
-        </ul>
+        </ul> */}
       </DndProvider>
     </InventoryContainer>
   );
